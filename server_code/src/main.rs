daf1263bba38af767ed8518cb8e36aec5c7b7880
Ciaran Doherty <cad106uk@gmail.com>
2020-06-27 20:05:49 +0100

    read bytes into json obect
    stopping here because the docs.rs site went down

--- server_code/src/main.rs
+++ server_code/src/main.rs
@@ -1,5 +1,26 @@
+use serde_json::Value;
+use tokio::io::File;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+use tokio::sync::mpsc;
+
+trait RecordJson {
+    fn append_json_row(&mut self, input: Vec<u8>) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+impl RecordJson for File {
+    async fn append_json_row(&mut input: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+        let json_data: Value = match serde_json::from_slice(&input) {
+            Ok(json_data) => json_data,
+            Err(e) => {
+                eprintln!("Invalid json data; err = {:?}", e);
+                return Err(Box::new(e));
+            }
+        };
+
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,13 +30,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
+            let mut input_vec: Vec<u8> = Vec::new();
             let mut buf = [0; 1024];
 
             // In a loop, read data from the socket and write the data back.
             loop {
-                let _ = match socket.read(&mut buf).await {
+                let n = match socket.read(&mut buf).await {
                     // socket closed
-                    Ok(n) if n == 0 => return,
+                    Ok(n) if n == 0 => break,
                     Ok(n) => n,
                     Err(e) => {
                         eprintln!("failed to read from socket; err = {:?}", e);
@@ -23,16 +45,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     }
                 };
 
-                let line = match String::from_utf8(buf.to_vec()) {
-                    Ok(line) => line,
-                    Err(e) => {
-                        eprintln!("Invalid utf8; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                println!("{}", line);
+                input_vec.extend_from_slice(&buf[..n]);
             }
+
+            let res = record_input(input_vec).await?;
         });
     }
 }
