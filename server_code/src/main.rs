da62bbe22d623ba6312acf8d8e5b4d0fa92336a6
Ciaran Doherty <cad106uk@gmail.com>
2020-06-27 13:36:26 +0100

    Simple example of server side receiving tcp streams

--- server_code/src/main.rs
+++ server_code/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:6969").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let _ = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                let line = match String::from_utf8(buf.to_vec()) {
+                    Ok(line) => line,
+                    Err(e) => {
+                        eprintln!("Invalid utf8; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                println!("{}", line);
+            }
+        });
+    }
 }
